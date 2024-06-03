3804f3f7dbaa1b16c41acd84f8cccffa89ea4d98
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-06-03 11:51:14 -0300

    Finished CarrinhoDeCompras exercise

--- src/list/basico/CarrinhoDeCompras.java
+++ src/list/basico/CarrinhoDeCompras.java
@@ -1,4 +1,58 @@
 package list.basico;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.DoubleToIntFunction;
+
 public class CarrinhoDeCompras {
+    private final List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        itemList.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome) {
+        List<Item> itensParaRemover = new ArrayList<>();
+
+        for (Item item : itemList) {
+            if (item.getNome().equalsIgnoreCase(nome)) {
+                itensParaRemover.add(item);
+            }
+        }
+
+        itemList.removeAll(itensParaRemover);
+    }
+
+    public double calcularValorTotal() {
+        double total = 0;
+
+        for (Item item : itemList) {
+            total += item.getPreco() * item.getQuantidade();
+        }
+
+        return total;
+    }
+
+    public void exibirItens() {
+        for (Item item : itemList) {
+            System.out.println("Item: " + item.getNome());
+            System.out.println("Preço: R$" + item.getPreco());
+            System.out.println("Quantidade: " + item.getQuantidade());
+            System.out.printf("Subtotal: R$%.2f\n\n", (item.getPreco() * item.getQuantidade()));
+        }
+    }
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+
+        carrinho.adicionarItem("Red Bull 200ml Tradicional", 9.49, 2);
+        carrinho.adicionarItem("Torcidas 70g Pimenta Mexicana", 2.49, 5);
+
+        carrinho.exibirItens();
+        System.out.printf("Valor total: R$%.2f", carrinho.calcularValorTotal());
+    }
 }
